c8fe4549cf755d93f892c7bb8dbd0eaabc30a858
darkitxu84 <76633266+darkitxu84@users.noreply.github.com>
2025-11-13 17:55:06 -0300

    add files

---
+++ src/NovellusLib/FileSystems/CriCPK.cs
@@ -0,0 +1,32 @@
+﻿using CriFsV2Lib;
+using CriFsV2Lib.Definitions.Interfaces;
+using CriFsV2Lib.Definitions.Structs;
+using CriFsV2Lib.Definitions.Utilities;
+using CriFsV2Lib.Encryption.Game;
+
+namespace NovellusLib.FileSystems
+{
+    /// <summary> Class for handling CriCPK file system operations.
+    /// <para>Used in most of the Atlus games after Persona 3 Portable</para>
+    /// <para>For more info, see: <see href="https://amicitia.miraheze.org/wiki/CPK"></see></para>
+    /// </summary>
+    public static class CriCPK
+    {
+        private class FileToExtract : IBatchFileExtractorItem
+        {
+            public string FullPath { get; set; }
+            public CpkFile File { get; set; }
+            public FileToExtract(string _fullPath, CpkFile _file)
+            {
+                FullPath = _fullPath;
+                File = _file;
+            }
+        }
+        // maybe we need some benchmarking later, fileList maybe can be optimized with HashSet
+        // cuz we're doing Contains checks for every file
+        public static void Unpack(string cpkPath, string output, string[]? fileList = null)
+        {
+
+        }
+    }
+}
---
+++ src/NovellusLib/FileSystems/PSPElf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovellusLib.FileSystems
+{
+    public static class PSPElf
+    {
+        public static void Decrypt(string elfPath, string outputPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
