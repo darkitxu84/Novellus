3278ad280acefbceb3024a4e15b0b5c3461616b7
darkitxu84 <76633266+darkitxu84@users.noreply.github.com>
2026-05-24 11:30:16 -0400

    optimize spr merging
    don't allocate the texture in memory, just use the file stream instead

--- src/Novellus.Lib.Backend/FileSystems/SPR.cs
+++ src/Novellus.Lib.Backend/FileSystems/SPR.cs
@@ -97,13 +97,13 @@ public static class SPR
     public static MemoryStream MergeSpr(Stream ogSprStream, List<(string TmxName, string SourcePath)> files, string sprName)
     {
         // we're going to use a memory stream to avoid writing/reading disk
-        var sprStreamMemory = new MemoryStream();
-        ogSprStream.CopyTo(sprStreamMemory);
-        sprStreamMemory.Position = 0;
+        var sprStream = new MemoryStream();
+        ogSprStream.CopyTo(sprStream);
+        sprStream.Position = 0;
         ogSprStream.Position = 0;
 
         // name -> offset
-        var sprTxmNames = GetTmxNames(sprStreamMemory);
+        var sprTxmNames = GetTmxNames(sprStream);
 
         // small buffer for reading tmx lengths
         Span<byte> buffer = stackalloc byte[4];
@@ -118,63 +118,47 @@ public static class SPR
             }
             Logger.Info($"Merging '{tmxName}' onto '{sprName}'");
 
-            // if we have a lot of textures it's better to use ArrayPool to avoid large allocations on the LOH
-            var fileInfo = new FileInfo(SourcePath);
-            int repTmxLen = (int)fileInfo.Length;
-            byte[] tmxBytes = ArrayPool<byte>.Shared.Rent(repTmxLen);
+            using var txmStream = File.OpenRead(SourcePath);
+            int repTmxLen = (int)txmStream.Length;
 
+            sprStream.Position = tmxOffset + 4;
+            sprStream.Read(buffer);
+            int ogTmxLen = BitConverter.ToInt32(buffer);
+
+            // early continue if the lengths are the same, we can just overwrite the tmx
+            if (repTmxLen == ogTmxLen)
+            {
+                sprStream.Position = tmxOffset;
+                txmStream.CopyTo(sprStream);
+                continue;
+            }
+
+            // if the lengths are different, we need to shift the rest of the file
+            int tailStart = tmxOffset + ogTmxLen;
+            int tailLen = (int)(sprStream.Length - tailStart);
+
+            byte[] tail = ArrayPool<byte>.Shared.Rent(tailLen);
             try
             {
-                using (var stream = File.OpenRead(SourcePath))
-                    stream.ReadExactly(tmxBytes, 0, repTmxLen);
+                sprStream.Position = tailStart;
+                sprStream.ReadExactly(tail, 0, tailLen);
 
-                sprStreamMemory.Position = tmxOffset + 4;
-                sprStreamMemory.Read(buffer);
-                int ogTmxLen = BitConverter.ToInt32(buffer);
-
-                if (repTmxLen == ogTmxLen)
-                {
-                    sprStreamMemory.Position = tmxOffset;
-                    sprStreamMemory.Write(tmxBytes, 0, repTmxLen);
-                }
-                else
-                {
-                    int oldSize = (int)sprStreamMemory.Length;
-                    int newSize = oldSize + (repTmxLen - ogTmxLen);
-                    byte[] newSpr = ArrayPool<byte>.Shared.Rent(newSize);
-
-                    try
-                    {
-                        sprStreamMemory.Position = 0;
-                        sprStreamMemory.Read(newSpr, 0, tmxOffset);
-
-                        tmxBytes.AsSpan(0, repTmxLen).CopyTo(newSpr.AsSpan(tmxOffset));
-
-                        int tailStart = tmxOffset + ogTmxLen;
-                        int tailLen = oldSize - tailStart;
-                        sprStreamMemory.Position = tailStart;
-                        sprStreamMemory.Read(newSpr, tmxOffset + repTmxLen, tailLen);
-
-                        sprStreamMemory.SetLength(0);
-                        sprStreamMemory.Write(newSpr, 0, newSize);
-                    }
-                    finally
-                    {
-                        ArrayPool<byte>.Shared.Return(newSpr);
-                    }
-                    sprStreamMemory.Position = 0;
-                    UpdateOffsets(sprStreamMemory, GetTmxOffsets(sprStreamMemory));
-                    sprStreamMemory.Position = 0;
-                    sprTxmNames = GetTmxNames(sprStreamMemory);
-                }
+                sprStream.SetLength(tmxOffset);
+                sprStream.Position = tmxOffset;
+                txmStream.CopyTo(sprStream);
+                sprStream.Write(tail, 0, tailLen);
             }
             finally
             {
-                ArrayPool<byte>.Shared.Return(tmxBytes);
+                ArrayPool<byte>.Shared.Return(tail);
             }
+            sprStream.Position = 0;
+            UpdateOffsets(sprStream, GetTmxOffsets(sprStream));
+            sprStream.Position = 0;
+            sprTxmNames = GetTmxNames(sprStream);
         }
 
-        sprStreamMemory.Position = 0;
-        return sprStreamMemory;
+        sprStream.Position = 0;
+        return sprStream;
     }
 }
